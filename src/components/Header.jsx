335f96bece4368bf3598fccffa26a29cd7686e79
mkeloo <mkeloo@ufl.edu>
2022-09-17 23:06:06 -0400

    updates to files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,23 +10,23 @@ import { HiMenu } from 'react-icons/hi';
 const Header = ({ setNavMobile }) => {
   return (
     <header
-      className='py-6'
-      data-aos='fade-down'
-      data-aos-duration='2000'
-      data-aos-delay='900'
+      className="py-6"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+      data-aos-delay="900"
     >
-      <div className='container mx-auto'>
-        <div className='flex items-center justify-between'>
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
           {/* logo */}
-          <a href='#'>
-            <img className='h-[30px]' src={Logo} alt='' />
+          <a href="/">
+            <img className="h-[30px]" src={Logo} alt="" />
           </a>
           {/* nav */}
           <Nav />
           {/* nav mobile btn */}
           <HiMenu
             onClick={() => setNavMobile(true)}
-            className='lg:hidden text-3xl text-white cursor-pointer'
+            className="lg:hidden text-3xl text-white cursor-pointer"
           />
         </div>
       </div>
--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -4,8 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-// import required modules
-import { Pagination } from 'swiper';
+
 // import data
 import { slider } from '../data';
 
@@ -32,17 +31,17 @@ const TestimonialSlider = () => {
         const { message, image, name, email } = slide;
         return (
           <SwiperSlide
-            className='rounded-lg p-4 lg:p-6 min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]'
+            className="rounded-lg p-4 lg:p-6 min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]"
             key={index}
           >
-            <p className='mb-8 min-h-[100px] text-[15px]'>{message}</p>
-            <div className='flex flex-col gap-x-4 md:flex-row md:items-center'>
-              <div className='w-14'>
-                <img className='mb-3 md:mb-0' src={image} alt='' />
+            <p className="mb-8 min-h-[100px] text-[15px]">{message}</p>
+            <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
+              <div className="w-14">
+                <img className="mb-3 md:mb-0" src={image} alt="" />
               </div>
               <div>
-                <div className='font-medium text-base'>{name}</div>
-                <div className='font-medium text-rose-600'>{email}</div>
+                <div className="font-medium text-base">{name}</div>
+                <div className="font-medium text-rose-600">{email}</div>
               </div>
             </div>
           </SwiperSlide>
